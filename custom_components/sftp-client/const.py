864748094d7b943a52956fe763b2d6554139247b
cyr-ius <cyr-ius@ipocus.net>
2025-06-20 18:45:59 +0200

    Up

--- custom_components/sftp-client/const.py
+++ custom_components/sftp-client/const.py
@@ -4,7 +4,7 @@ from collections.abc import Callable
 
 from homeassistant.util.hass_dict import HassKey
 
-DOMAIN = "sftp_client"
+DOMAIN = "sftp-client"
 CONF_BACKUP_PATH = "backup"
 DEFAULT_BACKUP_PATH = "backup"
 
